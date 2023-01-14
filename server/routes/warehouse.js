28c3a34568e1a78b76fc5c443eed752d077a78bf
Leo Nascimento <95266043+leo-sn@users.noreply.github.com>
2023-01-14 11:23:09 -0800

    added GET to a single warehouse returning warehouse data plus inventory for that specific warehouse

--- server/routes/warehouse.js
+++ server/routes/warehouse.js
@@ -3,6 +3,7 @@ const app = express();
 const router = express.Router(); // using routing to the server.js
 const fs = require('fs');
 const warehousesJSON = './data/warehouses.json'
+const inventoryJSON = './data/inventories.json'
 
 //MIDDLEWARE
 app.use(express.json());
@@ -11,6 +12,8 @@ app.use(express.json());
 //***** START HERE *****//
 //**********************//
 
+//***** FUNCTIONS ******/
+
 //read files
 function readWarehouse() {
     const warehouseFile = fs.readFileSync(warehousesJSON);
@@ -18,11 +21,65 @@ function readWarehouse() {
     return warehouseData;
 }
 
+function readInventory() {
+    const inventoryFile = fs.readFileSync(inventoryJSON);
+    const inventoryData = JSON.parse(inventoryFile)
+    return inventoryData;
+}
+
+//get the warehouse ID from URL
+function requestedID (req) {
+    return requestId = req.params.id
+}
+
+//get all inventory for specific warehouse
+function getInventoryFromWarehouse(id) {
+    const arr = []
+    const data = readInventory()
+    data.forEach(element => {
+        if(element.warehouseID === id) {
+            const obj = {
+                itemId: element.id,
+                itemName: element.itemName,
+                itemCategory: element.category,
+                itemStatus: element.status,
+                itemQuantity: element.quantity
+            }
+            arr.push(obj)
+        }
+    });
+    
+    return arr
+}
+
+function getWarehouseInfo(warehouseId) {
+   const warehouseData = readWarehouse();
+   const result = warehouseData.find( ({id}) => id === warehouseId)
+
+   return result
+}
+
+
+
+//***** REQUESTS ******/
 router.get('/', (_req, res) => {
-    warehouseData = readWarehouse();
+    const warehouseData = readWarehouse();
     res.status(200).send(warehouseData)
 })
 
+
+router.get('/:id', (req, res) => {
+    
+    const warehouseInfo = getWarehouseInfo(requestedID(req))
+
+    if(!warehouseInfo) {
+        return res.status(400).send('Warehouse ID must be valid')
+    }
+
+    const inventory = getInventoryFromWarehouse(requestedID(req));
+    const data = {...warehouseInfo, inventory}
+    res.status(200).json(data)
+})
 
 //EXPORTING
 module.exports = router; // exporting videos
