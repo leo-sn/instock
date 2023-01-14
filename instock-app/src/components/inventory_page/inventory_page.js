7262358bc1f7194d66bae595d8360bb246d37095
shrusred <reddy.shruthi35@gmail.com>
2023-01-15 04:25:58 +0530

    added components folder for the front end and update gitignore

---
+++ instock-app/src/components/inventory_page/inventory_page.js
@@ -0,0 +1,8 @@
+function inventory_page() {
+  return (
+    <div className="inventory_page">
+      <h2 className="inventory_page_title">INVENTORY PAGE</h2>
+    </div>
+  );
+}
+export default inventory_page;
