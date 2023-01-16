62e66b88b684245b64c1f63d4bdb73c518d26ecd
Amandeep Kaur <deepamankaur97@gmail.com>
2023-01-16 13:25:31 -0500

    Add responsive styles to footer and search component

--- instock-app/src/pages/Warehouse/Warehouse.js
+++ instock-app/src/pages/Warehouse/Warehouse.js
@@ -2,12 +2,14 @@ import Header from "../../components/Warehouse/Header/Header";
 import WarehouseList from "../../components/Warehouse/WarehouseList/WarehouseList";
 import axios from "axios";
 import { useState, useEffect } from "react";
+import Footer from "../../components/Warehouse/Footer/Footer";
 
 function Warehouse() {
 	return (
 		<>
 			<Header />
 			<WarehouseList />
+			<Footer />
 		</>
 	);
 }
