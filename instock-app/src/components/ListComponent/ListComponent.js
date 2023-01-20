20536085cc4cd05f84883180bcd1f844d471404d
shrusred <reddy.shruthi35@gmail.com>
2023-01-20 20:28:47 +0530

    added separate divs for mobile view alone

--- instock-app/src/components/ListComponent/ListComponent.js
+++ instock-app/src/components/ListComponent/ListComponent.js
@@ -18,40 +18,77 @@ function InventoryListComponent(props) {
   console.log(props.listitems);
 
   return (
-    <div className="inventory_listitem_td">
-      <div className="inventory_listitem_td__text">
-        <p>Placeholder1_inventoryitem</p>
-        <p>Placeholder1_category</p>
-        <p>Placeholder1_status</p>
-        <p>Placeholder1_qty</p>
-        <p>Placeholder1_warehouse</p>
-      </div>
+    <>
+      <div className="inventory_listitem_td">
+        <div className="inventory_listitem_td__text">
+          <p>Placeholder1_inventoryitem</p>
+          <p>Placeholder1_category</p>
+          <p>Placeholder1_status</p>
+          <p>Placeholder1_qty</p>
+          <p>Placeholder1_warehouse</p>
+        </div>
 
-      <div className="inventory_listitem_td__actionButtons">
-        <a
-          onClick={() => {
-            deleteInventoryItem("83433026-ca32-4c6d-bd86-a39ee8b7303e");
-          }}
-          className="inventory_listitem_td__actionButtons--deletebutton"
-        >
-          <img
-            className="inventory_listitem_td__actionButtons--delete"
-            src={del}
-          ></img>
-        </a>
-        <a
-          onClick={() => {
-            editInventoryItem("83433026-ca32-4c6d-bd86-a39ee8b7303e");
-          }}
-          className="inventory_listitem_td__actionButtons--editbutton"
-        >
-          <img
-            className="inventory_listitem_td__actionButtons--edit"
-            src={edit}
-          ></img>
-        </a>
+        <div className="inventory_listitem_td__actionButtons">
+          <a
+            onClick={() => {
+              deleteInventoryItem("83433026-ca32-4c6d-bd86-a39ee8b7303e");
+            }}
+            className="inventory_listitem_td__actionButtons--deletebutton"
+          >
+            <img
+              className="inventory_listitem_td__actionButtons--delete"
+              src={del}
+            ></img>
+          </a>
+          <a
+            onClick={() => {
+              editInventoryItem("83433026-ca32-4c6d-bd86-a39ee8b7303e");
+            }}
+            className="inventory_listitem_td__actionButtons--editbutton"
+          >
+            <img
+              className="inventory_listitem_td__actionButtons--edit"
+              src={edit}
+            ></img>
+          </a>
+        </div>
       </div>
-    </div>
+      <div className="inventory_listitem">
+        <div className="inventory_listitem__text">
+          <p>Placeholder1_inventoryitem</p>
+          <p>Placeholder1_category</p>
+          <p>Placeholder1_status</p>
+          <p>Placeholder1_qty</p>
+          <p>Placeholder1_warehouse</p>
+        </div>
+
+        <div className="inventory_listitem__actionButtons">
+          <a
+            onClick={() => {
+              deleteInventoryItem("83433026-ca32-4c6d-bd86-a39ee8b7303e");
+            }}
+            className="inventory_listitem__actionButtons--deletebutton"
+          >
+            <img
+              className="inventory_listitem_td__actionButtons--delete"
+              src={del}
+            ></img>
+          </a>
+          <a
+            onClick={() => {
+              editInventoryItem("83433026-ca32-4c6d-bd86-a39ee8b7303e");
+            }}
+            className="inventory_listitem__actionButtons--editbutton"
+          >
+            <img
+              className="inventory_listitem__actionButtons--edit"
+              src={edit}
+            ></img>
+          </a>
+        </div>
+      </div>
+    </>
   );
 }
+
 export default InventoryListComponent;
