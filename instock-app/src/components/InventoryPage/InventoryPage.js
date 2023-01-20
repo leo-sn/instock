63a4ab95c38a2ec93cddffb776001fc21f95c7cd
shrusred <reddy.shruthi35@gmail.com>
2023-01-20 14:00:06 +0530

    added updated html elements and scss for the list item component

--- instock-app/src/components/InventoryPage/InventoryPage.js
+++ instock-app/src/components/InventoryPage/InventoryPage.js
@@ -2,14 +2,19 @@ import "../ListComponent/ListComponent.scss";
 import ListComponent from "../ListComponent/ListComponent.js";
 import { useState, useEffect } from "react";
 import inventory_listitems from "../../assets/datatest/inventories.json";
+import DeleteInventoryItem from "../../components/InventoryDeleteModal/InventoryDeleteModal.js";
 
 function InventoryPage() {
+  //add new inventory item modal
+
   const [listitems, setListitems] = useState([]);
   console.log(inventory_listitems);
+  setListitems(inventory_listitems);
   return (
     <>
       <div className="page_box">
         <ListComponent listitems={listitems} />
+        <DeleteInventoryItem deleteitem={id} />
       </div>
     </>
   );
--- instock-app/src/components/ListComponent/ListComponent.js
+++ instock-app/src/components/ListComponent/ListComponent.js
@@ -2,27 +2,56 @@ import "../ListComponent/ListComponent.scss";
 import { Link } from "react-router-dom";
 import del from "../../assets/icons/delete_outline-24px.svg";
 import edit from "../../assets/icons/edit-24px.svg";
+import "../ListComponent/ListComponent.scss";
 
-function ListComponent(props) {
+function InventoryListComponent(props) {
+  //1. add delete function
+  function deleteInventoryItem(id) {
+    console.log(id);
+  }
+
+  //2. add edit function
+  function editInventoryItem(id) {
+    console.log(id);
+  }
+
   console.log(props.listitems);
+
   return (
-    <div className="inventory_listitem">
-      <p>Placeholder1_inventoryitem</p>
-      <p>Placeholder1_category</p>
-      <p>Placeholder1_status</p>
-      <p>Placeholder1_qty</p>
-      <p>Placeholder1_warehouse</p>
-      <div className="inventory_listitem__actionButtons">
-        <img
-          className="inventory_listitem__actionButtons--delete"
-          src={del}
-        ></img>
-        <img
-          className="inventory_listitem__actionButtons--edit"
-          src={edit}
-        ></img>
+    <div className="inventory_listitem_td">
+      <div className="inventory_listitem_td__text">
+        <p>Placeholder1_inventoryitem</p>
+        <p>Placeholder1_category</p>
+        <p>Placeholder1_status</p>
+        <p>Placeholder1_qty</p>
+        <p>Placeholder1_warehouse</p>
+      </div>
+
+      <div className="inventory_listitem_td__actionButtons">
+        <a
+          onClick={() => {
+            deleteInventoryItem("83433026-ca32-4c6d-bd86-a39ee8b7303e");
+          }}
+          className="inventory_listitem_td__actionButtons--deletebutton"
+        >
+          <img
+            className="inventory_listitem_td__actionButtons--delete"
+            src={del}
+          ></img>
+        </a>
+        <a
+          onClick={() => {
+            editInventoryItem("83433026-ca32-4c6d-bd86-a39ee8b7303e");
+          }}
+          className="inventory_listitem_td__actionButtons--editbutton"
+        >
+          <img
+            className="inventory_listitem_td__actionButtons--edit"
+            src={edit}
+          ></img>
+        </a>
       </div>
     </div>
   );
 }
-export default ListComponent;
+export default InventoryListComponent;
