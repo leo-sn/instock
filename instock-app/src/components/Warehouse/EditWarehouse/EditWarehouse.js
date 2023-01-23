37fd371dc8f7fe2b753a9a2640acfc3101669eca
Amandeep Kaur <deepamankaur97@gmail.com>
2023-01-23 10:01:01 -0500

    Add css for warehouse inventory list

--- instock-app/src/components/Warehouse/EditWarehouse/EditWarehouse.js
+++ instock-app/src/components/Warehouse/EditWarehouse/EditWarehouse.js
@@ -12,29 +12,20 @@ function EditWarehouse() {
 	let { warehouseId } = useParams();
 	console.log(warehouseId);
 
-	const [warehouse, setwarehouse] = useState({
+	const [warehouse, setWarehouse] = useState({
 		name: "",
 		address: "",
 		city: "",
 		country: "",
 		contactname: "",
 		position: "",
-		phoneNumber: "",
+		phone: "",
 		email: "",
 	});
 
-	const {
-		name,
-		address,
-		city,
-		country,
-		contactname,
-		position,
-		phoneNumber,
-		email,
-	} = warehouse;
 	const onInputChange = (e) => {
-		setwarehouse({ ...warehouse, [e.target.name]: e.target.value });
+		console.log(e);
+		setWarehouse({ ...warehouse, [e.target.name]: e.target.value });
 	};
 
 	useEffect(() => {
@@ -43,10 +34,25 @@ function EditWarehouse() {
 
 	const onSubmit = async (e) => {
 		e.preventDefault();
+
+		//add both inputs together
+		const toSubmitData = {
+			name: warehouse.name,
+			address: warehouse.address,
+			city: warehouse.city,
+			country: warehouse.country,
+			contact: {
+				name: warehouse.contactname,
+				position: warehouse.position,
+				phone: warehouse.phone,
+				email: warehouse.email,
+			},
+		};
+
 		console.log(e);
 		await axios.put(
 			`http://localhost:3030/warehouse/${warehouseId}`,
-			warehouse
+			toSubmitData
 		);
 		navigate("/");
 	};
@@ -55,7 +61,17 @@ function EditWarehouse() {
 		const result = await axios.get(
 			`http://localhost:3030/warehouse/${warehouseId}`
 		);
-		setwarehouse(result.data);
+		const warehouseData = {
+			name: result.data.name,
+			address: result.data.address,
+			city: result.data.city,
+			country: result.data.country,
+			contactname: result.data.contact.name,
+			position: result.data.contact.position,
+			phone: result.data.contact.phone,
+			email: result.data.contact.email,
+		};
+		setWarehouse(warehouseData);
 	};
 
 	return (
@@ -71,8 +87,10 @@ function EditWarehouse() {
 						type="text"
 						required="required"
 						name="name"
+						id="name"
 						// placeholder=""
-						value={name}
+
+						value={warehouse.name}
 						onChange={(e) => onInputChange(e)}
 					></input>
 					<label>Street Address</label>
@@ -81,8 +99,9 @@ function EditWarehouse() {
 						type="text"
 						required="required"
 						name="address"
+						id="address"
 						// placeholder=""
-						value={address}
+						value={warehouse.address}
 						onChange={(e) => onInputChange(e)}
 					></input>
 					<label>City</label>
@@ -91,8 +110,9 @@ function EditWarehouse() {
 						type="text"
 						required="required"
 						name="city"
+						id="city"
 						// placeholder=""
-						value={city}
+						value={warehouse.city}
 						onChange={(e) => onInputChange(e)}
 					></input>
 					<label>country</label>
@@ -101,8 +121,9 @@ function EditWarehouse() {
 						type="text"
 						required="required"
 						name="country"
+						id="country"
 						// placeholder=""
-						value={country}
+						value={warehouse.country}
 						onChange={(e) => onInputChange(e)}
 					></input>
 
@@ -113,9 +134,10 @@ function EditWarehouse() {
 						placeholder=""
 						type="text"
 						required="required"
-						name="contactName"
+						name="contactname"
+						id="contactname"
 						// placeholder=""
-						value={contactname}
+						value={warehouse.contactname}
 						onChange={(e) => onInputChange(e)}
 					></input>
 					<label>Position</label>
@@ -124,8 +146,9 @@ function EditWarehouse() {
 						type="text"
 						required="required"
 						name="position"
+						id="position"
 						// placeholder=""
-						value={position}
+						value={warehouse.position}
 						onChange={(e) => onInputChange(e)}
 					></input>
 					<label>Phone Number</label>
@@ -133,9 +156,10 @@ function EditWarehouse() {
 						placeholder=""
 						type="text"
 						required="required"
-						name="phoneNumber"
+						name="phone"
+						id="phone"
 						// placeholder=""
-						value={phoneNumber}
+						value={warehouse.phone}
 						onChange={(e) => onInputChange(e)}
 					></input>
 					<label>Email</label>
@@ -144,8 +168,9 @@ function EditWarehouse() {
 						type="text"
 						required="required"
 						name="email"
+						id="email"
 						// placeholder=""
-						value={email}
+						value={warehouse.email}
 						onChange={(e) => onInputChange(e)}
 					></input>
 
