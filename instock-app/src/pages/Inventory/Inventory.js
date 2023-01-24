184987124e5eb660280dbd6cc92a788284cada47
shrusred <reddy.shruthi35@gmail.com>
2023-01-24 09:35:57 +0530

    updated scss

--- instock-app/src/pages/Inventory/Inventory.js
+++ instock-app/src/pages/Inventory/Inventory.js
@@ -1,13 +1,22 @@
 import InventoryPage from "../../components/InventoryPage/InventoryPage.js";
+import axios from "axios";
+import { useState, useEffect } from "react";
 import Footer from "../../components/Warehouse/Footer/Footer.js";
 import Header from "../../components/Warehouse/Header/Header.js";
 function Inventory(props) {
-  console.log(props);
-  console.log("title" + props.title);
+  const title = props.title;
+  const [inventoryList, setinventoryList] = useState([]);
+  useEffect(() => {
+    axios.get("http://localhost:3030/inventory").then((res) => {
+      console.log("this is the inventory call");
+      console.log(res.data);
+      setinventoryList(res.data);
+    });
+  }, []);
   return (
     <>
       <Header />
-      <InventoryPage title={props.title} />
+      <InventoryPage inventoryList={inventoryList} title={title} />
       <Footer />
     </>
   );
